a44141f81f20f29a69c29dcccae2307cacfeb865
FonPay-Technologies <askmeidealintegratedservices@gmail.com>
2025-11-21 18:51:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from flask import Flask, render_template_string, request, jsonify
 from telegram import Update
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ChatMemberHandler
-from telegram.constants import ChatMemberStatus
 from telegram.ext import (
     Updater,
     CommandHandler,
